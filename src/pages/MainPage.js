3f25d45a60e024aba93ca5211fed37762f090cc1
mapcast <odri0220@naver.com>
2022-11-13 19:54:48 +0900

    first commit

---
+++ src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import Menu from '../components/Menu';
+
+const MainPage = () => {
+    return(
+        <div>
+            <Menu/>
+        </div>
+    )
+}
+
+export default MainPage;
